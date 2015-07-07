ada375befa2999daad1b75b7014b4473b9e25b08
Jonas Hecht <jonas.hecht@codecentric.de>
2015-07-07 10:12:44 +0200

    First running Test with needed Dependencies, but Problem with loading DMN-Decision-Table into DMN engine

--- src/test/java/de/jonashackt/dmndemo/DmndemoApplicationTests.java
+++ src/test/java/de/jonashackt/dmndemo/DmndemoApplicationTests.java
@@ -1,5 +1,9 @@
 package de.jonashackt.dmndemo;
 
+import static org.camunda.dmn.engine.test.asserts.DmnAssertions.assertThat;
+
+import org.camunda.dmn.engine.test.DecisionResource;
+import org.camunda.dmn.engine.test.DmnDecisionTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -7,11 +11,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DmndemoApplication.class)
-public class DmndemoApplicationTests {
+public class DmndemoApplicationTests extends DmnDecisionTest{
 
+    private static final String STATE_2_SHIP_DMN = "states2ship.dmn";
+    
 	@Test
-	@DecisionResource
-	public void contextLoads() {
+	@DecisionResource(resource = STATE_2_SHIP_DMN)
+	public void shouldEvaluateShipment() {
+	    assertThat(engine)
+	      .evaluates(decision)
+	      .withContext()
+	        .setVariable("state", "Afghanistan")
+	        .setVariable("product ", "RedCar")
+	        .build()
+	      .hasResult()
+	        .hasSingleOutput()
+	          .hasEntry("result", "notok")
+	          .hasEntry("reason", "sorry, no shipment possible");
+
 	}
 
 }
